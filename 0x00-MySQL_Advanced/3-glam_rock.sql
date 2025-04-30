c67489d7670ec6ce393d523492212957aa982609
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-04-30 19:30:56 +0000

    Modify 3-glam_rock.sql pattern match parameters

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,9 @@
 -- lists all bands with Glam rock as their main style ranked by longevity
 SELECT band_name,
-CASE
-	WHEN split IS NULL THEN 2022 - formed
-	ELSE split - formed
-END
-AS lifespan
+	CASE
+		WHEN split IS NULL THEN 2022 - formed
+		ELSE split - formed
+	END AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
